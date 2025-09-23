37bf859d70a4db3bf791bb726d0822e036de61d6
Andrey <yaroshenko_andrey2006@mail.ru>
2025-09-24 02:10:46 +0700

    add ExchangeQuote and ExchangeTransaction

---
+++ src/model/ExchangeTransaction.java
@@ -0,0 +1,25 @@
+package model;
+
+public class ExchangeTransaction {
+    private final ExchangeQuote quote;
+    private final double amountFrom;
+
+    public ExchangeTransaction(ExchangeQuote quote, double amountFrom) {
+        this.quote = quote;
+        this.amountFrom = amountFrom;
+    }
+
+    public ExchangeQuote getQuote() { return quote; }
+    public double getAmountFrom() { return amountFrom; }
+
+    public double computeAmountTo() {
+        return amountFrom * quote.rateWithCommission();
+    }
+
+    @Override
+    public String toString() {
+        return "транзакция: " + amountFrom + " " + quote.getCurrency1() +
+                " -> " + String.format("%.4f", computeAmountTo()) + " " + quote.getCurrency2() +
+                " (" + quote.getBank().toString() + ")";
+    }
+}
