5e5e85dc48156136627faf4ff200ce3b6b72b9a3
Andrey <yaroshenko_andrey2006@mail.ru>
2025-09-24 02:36:10 +0700

    update Main for demo

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,11 @@
 import io.ExchangeParser;
 import model.Bank;
+import model.ExchangeQuote;
 import model.ExchangeRate;
+import model.ExchangeTransaction;
 import service.AnalyticsService;
 
 import java.io.IOException;
-import java.text.MessageFormat;
 import java.text.ParseException;
 import java.util.List;
 import java.util.Optional;
@@ -16,6 +17,7 @@ public class Main {
 
         List<Bank> banks = exchangeParser.parseBanksFromFile("banks.txt");
         List<ExchangeRate> rates = exchangeParser.parseExchangeRatesFromFile("test1.txt");
+        List<ExchangeQuote> quotes = exchangeParser.parseQuotesFromFile("quotes.txt", banks);
 
         System.out.println("[БАНКИ]");
         banks.forEach(System.out::println);
@@ -30,5 +32,18 @@ public class Main {
         Optional<ExchangeRate> minRate = analyticsService.findMinRate(rates);
         System.out.println("\n[Минимальный курс]");
         minRate.ifPresent(System.out::println);
+
+        System.out.println("\n[КОТИРОВКИ]");
+        quotes.forEach(System.out::println);
+
+        Optional<ExchangeQuote> bestQuote = analyticsService.findBestQuote(quotes);
+        System.out.println("\n[ЛУЧШАЯ КОТИРОВКА (минимальный курс c комиссией)]");
+        bestQuote.ifPresent(System.out::println);
+
+        if (bestQuote.isPresent()) {
+            ExchangeTransaction trn = new ExchangeTransaction(bestQuote.get(), 150.00);
+            System.out.println("\n[ПРИМЕР ТРАНЗАКЦИИ]");
+            System.out.println(trn);
+        }
     }
 }
